9a6aa1c8c82adf02b53d4e0747dc48751667e619
LVVSys <Driver-DIch@mail.ru>
2023-11-24 01:57:10 +0300

    begin

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 21;
+        if (age >= 18) {
+            System.out.println("Ты можешь водить");
+        }
+
+        if (age < 18) {
+            System.out.println("Ты не можешь водить");
+        };
+
+    }
+}
